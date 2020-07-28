861c885f30d0e2354d1874a747029ea59cbdebb3
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-07-28 20:38:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,8 @@
 #write your code here
-number = 10
 def countdown(number)
   while number >= 0
-    countdown -= 1
     puts "#{number} SECOND(S)!"
+    countdown -= 1
   end
  puts "HAPPY NEW YEAR"
 end
